28dd46400dae99569767fcb1f66bb03259ce212e
PatasValparaiso <patasvalparaiso@gmail.com>
2025-05-05 12:22:21 -0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,11 @@
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Topo from "../components/Topo";
-import ListaAnimais from "../components/ListaAnimais";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-const App = () => {
-  return (
-    <div style={{ fontFamily: "sans-serif" }}>
-      <Topo />
-      <div style={{ display: "flex", padding: 20 }}>
-        <ListaAnimais />
-        <div style={{ flex: 1, marginLeft: 20 }}>
-          <p>Gráficos aqui</p>
-        </div>
-      </div>
-      <footer style={{ textAlign: "center", padding: 20 }}>
-        Criado por: Ronnor Oliveira
-      </footer>
-    </div>
-  );
-};
-
-const root = document.getElementById("root");
-ReactDOM.createRoot(root).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
